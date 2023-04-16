92ea4d9a879739afe1af70f5775d52ae4f148cd7
donniean <acmilank22@gmail.com>
2023-04-17 00:41:58 +0800

    ci: modify GitHub actions workflows (#13)

--- release.config.cjs
+++ release.config.cjs
@@ -6,6 +6,12 @@ module.exports = {
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: 'npx prettier --write "CHANGELOG.md"',
+      },
+    ],
     '@semantic-release/git',
   ],
 };
