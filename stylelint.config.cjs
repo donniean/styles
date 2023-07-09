2cd4484a4a517f399eb829f3b68fa0f1cfc00f43
donniean <acmilank22@gmail.com>
2023-07-09 17:48:30 +0800

    chore: update configs (#71)

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,10 +1,22 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind', 'layer', 'apply', 'config'],
+      },
+    ],
     'color-named': [
       'never',
       {
         ignore: ['inside-function'],
+      },
+    ],
+    'function-no-unknown': [
+      true,
+      {
+        ignoreFunctions: ['theme', 'screen'],
       },
     ],
     'no-unknown-animations': true,
